9df540c4e7754c924f035e0c863f9caabb3e633d
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-12-07 17:43:37 -0800

    Now installs a32nx to msfs

--- src/components/AircraftSection/index.tsx
+++ src/components/AircraftSection/index.tsx
@@ -2,7 +2,11 @@ import React, {useState} from 'react'
 import { Select, Typography } from 'antd';
 import { DownloadOutlined } from '@ant-design/icons';
 import { ButtonsContainer as SelectionContainer, Content, Container, HeaderImage, InstallButton, ModelInformationContainer, ModelName, ModelSmallDesc, AircraftModelSelect, VersionSelect, EngineOptionsContainer, EngineOption } from './styles';
-import * as fflate from 'fflate';
+import Store from 'electron-store';
+import * as fs from "fs";
+import Zip from 'adm-zip';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { app } = require('electron').remote;
 
 const { Option } = Select;
 const { Paragraph } = Typography;
@@ -27,14 +31,34 @@ const index: React.FC<{ aircraftModel: string }> = ({ aircraftModel }) => {
     ]
 
     async function downloada32nx(version: a32nxVersion) {
-        // const a32nxBuf = await fetch(version.url).then(
-        //     res => res.arrayBuffer()
-        // );
-//
-        // const a32nxFile = new Uint8Array(a32nxBuf);
-//
-        // const unzippeda32nx = fflate.unzipSync(a32nxFile);
+        const settings = new Store;
 
+        if (!isDownloading) {
+            setIsDownloading(true);
+            const msfs_package_dir = settings.get('mainSettings.msfsPackagePath');
+
+            const deleteHandle = fs.rmdir(msfs_package_dir + 'A32NX\\', {recursive: true}, () => {
+                console.log("Deleted original file");
+            });
+
+            const a32nxBuf = await fetch(version.url).then((res) => {
+                console.log("Starting Download");
+                return res.arrayBuffer();
+            });
+
+            const a32nxCompressed = Buffer.from(new Uint8Array(a32nxBuf));
+
+
+            if (typeof msfs_package_dir === "string") {
+                const a32nx = new Zip(a32nxCompressed);
+
+                await deleteHandle;
+
+                a32nx.extractAllTo(msfs_package_dir);
+            }
+            setIsDownloading(false);
+            console.log("Download complete!");
+        }
     }
 
     return (
