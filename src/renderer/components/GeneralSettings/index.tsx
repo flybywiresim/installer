488d22e815e4bc49c7a557a2c4802abdde3c3840
benjozork <benjamin.dupont.984@gmail.com>
2021-01-13 16:37:20 -0500

    feat: allow resetting install path
    Fixes #49

--- src/renderer/components/GeneralSettings/index.tsx
+++ src/renderer/components/GeneralSettings/index.tsx
@@ -1,47 +1,71 @@
 import React, { useState } from 'react';
 import Store from 'electron-store';
 import { setupInstallPath } from 'renderer/actions/install-path.utils';
-import { Container, PageTitle, SettingItemContent, SettingItemName, SettingsItem, SettingsItems, SettingButton } from './styles';
+import {
+    Container,
+    PageTitle,
+    SettingButton,
+    SettingItemContent,
+    SettingItemName,
+    SettingsItem,
+    SettingsItems
+} from './styles';
 import styled from "styled-components";
 import { colors } from "renderer/style/theme";
+import { configureInitialInstallPath } from "renderer/settings";
 
 const settings = new Store;
 
-function InstallPathSettingItem(): JSX.Element {
-    const [installPath, setInstallPath] = useState(settings.get('mainSettings.msfsPackagePath'));
-
+// eslint-disable-next-line no-unused-vars
+function InstallPathSettingItem(props: { path: string, setPath: (path: string) => void }): JSX.Element {
     async function handleClick() {
         const path = await setupInstallPath();
 
         if (path) {
-            setInstallPath(path);
+            props.setPath(path);
         }
     }
 
     return (
         <SettingsItem>
             <SettingItemName>Install Directory</SettingItemName>
-            <SettingItemContent>{installPath}</SettingItemContent>
+            <SettingItemContent>{props.path}</SettingItemContent>
             <SettingButton onClick={handleClick}>Modify</SettingButton>
         </SettingsItem>
     );
 }
 
-const VersionNumber = styled.h6`
+const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  
+  padding-right: .55em;
+`;
+
+const InfoItem = styled.h6`
   margin-top: 1.5em;
   color: ${colors.mutedText} !important;
 `;
 
 function index(): JSX.Element {
+    const [installPath, setInstallPath] = useState<string>(settings.get('mainSettings.msfsPackagePath') as string);
+
+    const handleReset = async () => setInstallPath(await configureInitialInstallPath());
+
     return (
         <>
             <Container>
                 <PageTitle>General Settings</PageTitle>
                 <SettingsItems>
-                    <InstallPathSettingItem />
+                    <InstallPathSettingItem path={installPath} setPath={setInstallPath} />
                 </SettingsItems>
             </Container>
-            <VersionNumber>{settings.get('metaInfo.currentVersion')}</VersionNumber>
+            <InfoContainer>
+                <InfoItem>{settings.get('metaInfo.currentVersion')}</InfoItem>
+                <InfoItem onClick={handleReset}>Reset</InfoItem>
+            </InfoContainer>
         </>
     );
 }
