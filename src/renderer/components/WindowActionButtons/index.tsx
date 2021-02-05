aa7178bfec1dfeff34310c632496623a7a33b2bc
Armankir <admin@iconicsim.com>
2021-02-04 23:55:18 -0500

    feat: added issue creation button
    Co-Authored-By: ZigTag <17077422+ZigTag@users.noreply.github.com>

--- src/renderer/components/WindowActionButtons/index.tsx
+++ src/renderer/components/WindowActionButtons/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { BorderOutlined, CloseOutlined, MinusOutlined } from '@ant-design/icons';
+import { BorderOutlined, CloseOutlined, MinusOutlined, ExclamationCircleOutlined } from '@ant-design/icons';
 import { Button, Container } from './styles';
 import InstallerUpdate from '../InstallerUpdate';
+import { shell } from 'electron';
 
 function index(): JSX.Element {
+
+    const openGithub = () => shell.openExternal("https://github.com/flybywiresim/a32nx/issues/new/choose");
+
     return (
         <Container>
             <InstallerUpdate />
+            <Button onClick={openGithub} ><ExclamationCircleOutlined /></Button>
             <Button id="min-button"><MinusOutlined /></Button>
             <Button id="max-button"><BorderOutlined /></Button>
             <Button id="close-button" isClose><CloseOutlined /></Button>
