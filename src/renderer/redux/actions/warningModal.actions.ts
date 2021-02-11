6c86783aca0cff8fc07ba7b871563282d443e5dc
ZigTag <17077422+ZigTag@users.noreply.github.com>
2021-02-11 13:13:26 -0800

    fix: made sure that it could never throw errors from null handle.

--- src/renderer/redux/actions/warningModal.actions.ts
+++ src/renderer/redux/actions/warningModal.actions.ts
@@ -2,10 +2,10 @@ import * as actionTypes from "../actionTypes";
 import { ShowWarningModalAction } from "../types";
 import { ModTrack } from "renderer/components/App";
 
-export function callWarningModal(show: boolean, track: ModTrack | null, setTrack?: boolean, findAndSetTrack?: CallableFunction): ShowWarningModalAction {
+export function callWarningModal(show: boolean, track: ModTrack | null, setTrack?: boolean, findAndSetTrack?: null | CallableFunction): ShowWarningModalAction {
     let trackHandle: null | CallableFunction = null;
 
-    if (typeof findAndSetTrack !== 'undefined') {
+    if (typeof findAndSetTrack !== 'undefined' && typeof findAndSetTrack !== null) {
         trackHandle = findAndSetTrack;
         if (typeof setTrack !== 'undefined' && setTrack) {
             findAndSetTrack(track.key);
