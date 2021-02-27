2ab51ea0c3a89a12c3f2120d242f717d20dede9b
ZigTag <17077422+ZigTag@users.noreply.github.com>
2021-02-26 16:07:42 -0800

    feat: added basic styling for the pr items

--- src/renderer/components/AircraftSection/QAPage.tsx
+++ src/renderer/components/AircraftSection/QAPage.tsx
@@ -4,17 +4,35 @@ import { GitVersions, PullInfo } from '@flybywiresim/api-client';
 type PRItemProps = {
     pull: PullInfo,
     isSelected: boolean,
-    setSelectedPull: CallableFunction,
+    setSelectedPull: () => void,
 }
 
 const PRItem = (props: PRItemProps) => {
     return (
-        <div className="flex flex-row text-white" onClick={() => props.setSelectedPull} key={props.pull.number}>
-            <p className="text-white ml-2">{props.pull.number}</p>
-            <p className="text-white ml-2">{props.pull.title}</p>
-            <p className="text-white ml-2">{props.pull.author}</p>
-            <p>{props.isSelected ? 'active' : 'unactive'}</p>
-        </div>
+        <>
+            <div
+                className="flex flex-row rounded-md mb-2 bg-card-bg border-card-bg"
+                key={props.pull.number}
+                onClick={() => props.setSelectedPull()}
+            >
+                {props.isSelected ?
+                    <div className="w-1 bg-card-active rounded-l-md" />
+                    :
+                    <div className="w-1 bg-card-inactive rounded-l-md" />
+                }
+                <div className="flex flex-row p-1">
+                    <p className="ml-1 mb-0 text-text-muted">{`#${props.pull.number}`}</p>
+                    <p className="ml-2 mb-0 text-text-normal">{props.pull.title}</p>
+                </div>
+                {props.isSelected ?
+                    <div className="flex bg-card-active w-20 rounded-r-md ml-auto items-center">
+                        <p className="flex-grow mb-0 text-center text-lg text-text-normal">Install</p>
+                    </div>
+                    : <></>
+                }
+            </div>
+
+        </>
     );
 };
 
@@ -24,7 +42,7 @@ const QAPage = () => {
 
     const RefreshPulls = () => {
         GitVersions.getPulls('flybywiresim', 'a32nx')
-            .then(r => setPulls(r));
+            .then((r: PullInfo[]) => setPulls(r.reverse()));
     };
 
     const isSelected = (pullNumber: number) => {
@@ -34,8 +52,13 @@ const QAPage = () => {
     return (
         <div className="m-6">
             <div className="flex flex-col">
-                <button type="button" className="text-white text-left" onClick={() => RefreshPulls()}>Refresh Pulls</button>
-                {pulls.reverse().map(pull =>
+                <div className="w-40 mb-4">
+                    <p
+                        className="text-white mb-0 text-center bg-card-bg rounded-md border-5"
+                        onClick={() => RefreshPulls()}
+                    >Refresh Pulls</p>
+                </div>
+                {pulls.map(pull =>
                     <PRItem
                         pull={pull}
                         isSelected={isSelected(pull.number)}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
+const defaultColors = require('tailwindcss/colors');
+
+const colors = {
+    ...defaultColors,
+    ...{
+        'card-bg': '#313131',
+        'card-active' : '#2E995E',
+        'card-inactive': '#5C5C5C',
+        'text-muted': '#868686',
+        'text-normal': '#FEFEFE',
+    }
+};
+
 module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
@@ -6,6 +19,7 @@ module.exports = {
                 'inner-sm': 'inset 0 1px 2px 0 rgba(0, 0, 0, 0.05)'
             }
         },
+        colors: colors
     },
     variants: {
         extend: {},
