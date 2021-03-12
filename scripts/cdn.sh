b4f6b011fdd6681f6027bb1cf099839f48d8bfc5
Niklas Steiner <niklas@sbg.at>
2021-03-12 17:45:26 +0100

    build: purge cdn after release

--- scripts/cdn.sh
+++ scripts/cdn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 CDN_URL="storage.bunnycdn.com/flybywiresim"
+CDN_PURGE_LINK="https://bunnycdn.com/api/purge?url=http://flybywiresim.b-cdn.net"
 FILES=${1}
 CDN_DIR=${2:-"installer/test"}
 
@@ -12,5 +13,13 @@ for FILE in "${FILES}"/*; do
     echo "Syncing file: $FILE"
     echo "Destination: $DEST"
     curl -X PUT -H "AccessKey: $BUNNY_BUCKET_PASSWORD" --data-binary "@$FILE" "$DEST"
-    echo ""; echo ""
+done
+
+# Purge after all uploads that the files are somewhat in sync
+echo "Purging cache"
+for FILE in "${FILES}"/*; do
+    DEST="$CDN_PURGE_LINK/$CDN_DIR/$(basename -- "$FILE")"
+    echo "Purging cache for file: $FILE"
+    echo "Purge URL: $DEST"
+    curl -X POST -H "AccessKey: $BUNNY_SECRET_TOKEN" -H "Content-Length: 0" "$DEST"
 done
