f5391ae8e30c87ae6eda76728aedad9fa95d88f3
benjozork <benjamin.dupont.984@gmail.com>
2021-03-30 16:38:18 -0400

    refactor: switch to electron-builder

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const rules = require('./webpack.rules');
+const devRules = require('./webpack.dev.rules');
 const plugins = require('./webpack.plugins');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 rules.push({
     test: /\.css$/,
     use: [
-        { loader: 'style-loader' },
-        { loader: 'css-loader' },
+        {
+            loader: 'style-loader'
+        },
+        {
+            loader: 'css-loader'
+        },
         {
             loader: "postcss-loader",
             options: {
@@ -22,10 +28,18 @@ rules.push({
 });
 
 module.exports = {
-    module: {
-        rules,
+    entry: './src/renderer/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist/renderer/'),
     },
-    plugins: plugins,
+    target: 'electron-renderer',
+    module: {
+        rules: [...rules, ...devRules],
+    },
+    plugins: [
+        ...plugins,
+        new HtmlWebpackPlugin(),
+    ],
     resolve: {
         alias: {
             'renderer': path.resolve(__dirname, './src/renderer'),
