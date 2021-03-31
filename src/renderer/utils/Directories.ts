4dc0da36856af5bffdd81b66468ff0336b04c706
Niklas Steiner <niklas@sbg.at>
2021-03-31 19:33:53 +0200

    refactor: more utility functions

--- src/renderer/utils/Directories.ts
+++ src/renderer/utils/Directories.ts
@@ -1,5 +1,7 @@
 import Store from "electron-store";
 import path from "path";
+import { Mod } from "renderer/utils/InstallerConfiguration";
+import fs from "fs-extra";
 
 const settings = new Store;
 
@@ -13,4 +15,44 @@ export class Directories {
         return path.join(settings.get('mainSettings.msfsPackagePath') as string, `flybywire_current_install_${(Math.random() * 1000).toFixed(0)}`);
     }
 
+    static removeAlternativesForMod(mod: Mod): void {
+        mod.alternativeNames?.forEach(altName => {
+            const altDir = Directories.inCommunity(altName);
+
+            if (fs.existsSync(altDir)) {
+                console.log('Removing alternative', altDir);
+                fs.rmdirSync(altDir, { recursive: true });
+            }
+        });
+    }
+
+    static removeTargetForMod(mod: Mod): void {
+        const dir = Directories.inCommunity(mod.targetDirectory);
+
+        if (fs.existsSync(dir)) {
+            console.log('Removing', dir);
+            fs.rmdirSync(dir, { recursive: true });
+        }
+    }
+
+    static isFragmenterInstall(target: string | Mod): boolean {
+        const targetDir = typeof target === 'string' ? target : Directories.inCommunity(target.targetDirectory);
+
+        return fs.existsSync(path.join(targetDir, 'install.json'));
+    }
+
+    static isGitInstall(target: string | Mod): boolean {
+        const targetDir = typeof target === 'string' ? target : Directories.inCommunity(target.targetDirectory);
+
+        try {
+            const symlinkPath = fs.readlinkSync(targetDir);
+            if (symlinkPath && fs.existsSync(path.join(symlinkPath, '/../.git'))) {
+                console.log('Is git repo', targetDir);
+                return true;
+            }
+        } catch {
+            console.log('Is not git repo', targetDir);
+            return false;
+        }
+    }
 }
---
+++ src/renderer/utils/Msfs.ts
@@ -0,0 +1,18 @@
+import net from "net";
+
+export class Msfs {
+    static async isRunning(): Promise<boolean> {
+        return new Promise((resolve) => {
+            const socket = net.connect(500);
+
+            socket.on('connect', () => {
+                resolve(true);
+                socket.destroy();
+            });
+            socket.on('error', () => {
+                resolve(false);
+                socket.destroy();
+            });
+        });
+    }
+}
