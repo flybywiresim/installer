9cab93122246c7d7a022d8f69491d323e1a6eeb6
Niklas Steiner <niklas@sbg.at>
2021-09-27 23:21:32 +0200

    refactor: remove unused code (#300)

--- src/renderer/components/App/styles.tsx
+++ src/renderer/components/App/styles.tsx
@@ -1,12 +1,5 @@
-import styled, { css } from 'styled-components';
-import { Layout, Menu } from 'antd';
-import { colors } from "renderer/style/theme";
-import React, { useEffect, useState } from "react";
-import Store from "electron-store";
-import fs from "fs-extra";
-import { Addon } from "renderer/utils/InstallerConfiguration";
-
-const settings = new Store<{ [p: string]: string }>({ watch: true });
+import styled from 'styled-components';
+import { Layout } from 'antd';
 
 export const { Content } = Layout;
 
@@ -21,42 +14,6 @@ export const PageHeader = styled.div`
   -webkit-app-region: drag;
   display: flex;
   justify-content: space-between;
-`;
-
-const menuItemCss = css`
-    margin: 0 !important;
-    height: 34px;
-    background-color: ${colors.listItem};
-    font-size: 16px;
-    font-weight: 600;
-    :hover {
-        background-color: #7e7e7e !important;
-    }
-`;
-
-export const MenuItem = styled(Menu.Item)`
-    display: flex;
-    align-items: center;
-    ${menuItemCss}
-`;
-
-export const AircraftSubMenuItem = styled(Menu.SubMenu)`
-    background-color: #444444 !important;
-    img {
-        height: 16px;
-        margin-right: 6px;
-    }
-
-    .ant-menu-submenu-title {
-        margin: 0;
-    }
-
-`;
-
-export const HomeMenuItem = styled(MenuItem)`
-    img {
-        height: 13px !important;
-    }
 `;
 
 export const PageSider = styled.div`
@@ -76,92 +33,3 @@ export const PageSider = styled.div`
 export const MainLayout = styled(Layout)`
     height: 100%;
 `;
-
-const AircraftDetailsContainer = styled.div`
-  display: flex !important;
-  flex-direction: row;
-  justify-content: space-between;
-  
-  img#icon-A32NX {
-    height: 173px !important;
-    position: absolute;
-    right: -192px;
-    top: -62px;
-  }
-
-  img#icon-A380X {
-    height: 172px !important;
-    position: absolute;
-    right: -180px;
-    top: -55px;
-  }
-`;
-
-const AircraftInfo = styled.div`
-  width: 22em;
-        
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  
-  margin-top: 3em;
-  margin-left: 14px;
-`;
-
-const AircraftName = styled.h4`
-  font-size: 18px;
-  margin-top: -2em;
-  line-height: 18px;
-`;
-
-const AircraftInstalledVersion = styled.h6`
-  font-size: 14px;
-  line-height: 18px;
-`;
-
-const AircraftMenuItemBase = styled(MenuItem)`
-    display: flex !important;
-    justify-content: space-between;
-    height: 73px !important;
-    padding-left: 8px !important;
-`;
-
-const InstallationStates = {
-    INSTALLED: 'installed',
-    NOT_INSTALLED: 'not installed'
-};
-
-type AircraftMenuItemProps = { addon: Addon, disabled: boolean };
-
-export const AircraftMenuItem = (props: AircraftMenuItemProps): JSX.Element => {
-    const getInstallText = (value: boolean) => value ? InstallationStates.INSTALLED : InstallationStates.NOT_INSTALLED;
-
-    const installDir = `${settings.get('mainSettings.msfsPackagePath')}\\${props.addon.targetDirectory}\\`;
-
-    let isInstalled = false;
-
-    try {
-        isInstalled = fs.existsSync(installDir);
-    } catch (e) {
-        console.error(e);
-    }
-
-    const [installationStatus, setInstallationStatus] = useState<string>(() => getInstallText(isInstalled));
-
-    useEffect(() => {
-        settings.onDidChange(`cache.${props.addon.key}.lastUpdated`, () => setInstallationStatus(getInstallText(true)));
-    });
-
-    return (
-        <AircraftMenuItemBase {...props}>
-            <AircraftDetailsContainer>
-                <AircraftInfo>
-                    <AircraftName>{props.addon.name}</AircraftName>
-                    <AircraftInstalledVersion>{props.addon.enabled ? installationStatus : "not available"}</AircraftInstalledVersion>
-                </AircraftInfo>
-                <img id={`icon-${props.addon.key}`} src={props.addon.menuIconUrl} alt={props.addon.aircraftName}/>
-            </AircraftDetailsContainer>
-        </AircraftMenuItemBase>
-    );
-};
