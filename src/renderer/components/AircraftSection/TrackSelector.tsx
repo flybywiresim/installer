ee1d7cff500cedbf9c33b9ccda9ef2b94f1619b3
GhostEagle68 <Ghosteagle68@gmail.com>
2021-11-08 18:47:53 -0700

    move latestVersionDate to  latestVersionName code

--- src/renderer/components/AircraftSection/TrackSelector.tsx
+++ src/renderer/components/AircraftSection/TrackSelector.tsx
@@ -56,8 +56,7 @@ export const Track: React.FC<TrackProps> = ({ isSelected, isInstalled, handleSel
                 className={`${makeBorderStyle()} w-1 h-12 rounded-r-xl transition-all duration-200 transform ${isSelected ? 'scale-y-100' : 'scale-y-50'}`}/>
             <div className="flex flex-col px-5 py-2">
                 <span className="text-xl text-gray-50">{track.name}</span>
-                <span className="text-lg text-teal-50 -mt-0.5"><code>{latestVersionName}</code></span>
-                <span className="text-lg text-teal-50 -mt-0.5"><code>{dateFormat(latestVersionDate, settings.get('mainSettings.dateLayout') as string)}</code></span>
+                <span className="text-lg text-teal-50 -mt-0.5"><code>{latestVersionName} {dateFormat(latestVersionDate, settings.get('mainSettings.dateLayout') as string)}</code></span>
             </div>
         </div>
     );
