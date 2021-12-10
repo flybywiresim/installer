7befa811da1463702f88aea8f9ec7509bd77f0eb
ErickSharp <titaninmc@gmail.com>
2021-12-09 20:41:54 -0600

    fix: include NoAvailableAddons component

---
+++ src/renderer/components/NoAvailableAddonsSection/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NoAvailableAddonsSection = () => (
+    <div className="flex items-center justify-center text-white w-full h-full text-xl">
+        There are currently no available addons provided by this publisher.
+    </div>
+);
