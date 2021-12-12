911bc41388d9be1452eab870879d28fb9491015c
ErickSharp <titaninmc@gmail.com>
2021-12-12 15:30:35 -0600

    refactor: use text-center

--- src/renderer/components/NoAvailableAddonsSection/index.tsx
+++ src/renderer/components/NoAvailableAddonsSection/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const NoAvailableAddonsSection = () => (
-    <div className="flex items-center justify-center text-white w-full h-full text-xl">
+    <div className="flex items-center justify-center text-center text-white w-full h-full px-4 text-xl">
         There are currently no available addons provided by this publisher.
     </div>
 );
