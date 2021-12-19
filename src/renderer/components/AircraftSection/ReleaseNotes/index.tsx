72588035e2640f3110c2db659f4ac7cb0bfcc4a2
ErickSharp <titaninmc@gmail.com>
2021-12-18 19:54:22 -0600

    feat: add dummy component when rendering release note markdown body

--- src/renderer/components/AircraftSection/ReleaseNotes/index.tsx
+++ src/renderer/components/AircraftSection/ReleaseNotes/index.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { useAppSelector } from "renderer/redux/store";
 
 export const ReleaseNotes = () => {
     const releases = useAppSelector(state => state.releaseNotes);
+    const [releaseComponent, setReleaseComponent] = useState<JSX.Element>(undefined);
 
-    return (
-        <div className="w-full h-full p-7 overflow-y-scroll">
-            <div className="flex flex-row items-center justify-between">
-                <h2 className="text-white font-extrabold">
-                    Release Notes
-                </h2>
-                {/*    Dropdown will go here*/}
-            </div>
+    useEffect(() => {
+        setReleaseComponent(
             <div className="flex flex-col gap-y-7">
                 {releases.map((release, index) =>
                     <div className="rounded-md bg-navy p-7" key={index}>
@@ -27,6 +22,29 @@ export const ReleaseNotes = () => {
                     </div>
                 )}
             </div>
+        );
+    }, []);
+
+    const DummyComponent = () => (
+        <div className="flex flex-col gap-y-7">
+            {[...Array(10)].map(_ => (
+                <div className="rounded-md bg-navy p-7 animate-pulse">
+                    <h3 className="bg-navy-light h-4 w-32"/>
+                    <div className="h-44 w-full bg-navy-light"/>
+                </div>
+            ))}
+        </div>
+    );
+
+    return (
+        <div className="w-full h-full p-7 overflow-y-scroll">
+            <div className="flex flex-row items-center justify-between">
+                <h2 className="text-white font-extrabold">
+                    Release Notes
+                </h2>
+                {/*    Dropdown will go here*/}
+            </div>
+            {releaseComponent ?? <DummyComponent/>}
         </div>
     );
 };
