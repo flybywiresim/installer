0f875f85076ef7ccdbe77643318935df8f2e6954
FoxtrotSierra6829 <github@florian-scheuner.de>
2021-12-30 23:07:27 +0100

    refactor: function return types missing

--- src/renderer/components/NoAvailableAddonsSection/index.tsx
+++ src/renderer/components/NoAvailableAddonsSection/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const NoAvailableAddonsSection = () => (
+export const NoAvailableAddonsSection = (): JSX.Element => (
     <div className="flex items-center justify-center text-center text-white w-full h-full px-4 text-xl">
         There are currently no available addons provided by this publisher.
     </div>
