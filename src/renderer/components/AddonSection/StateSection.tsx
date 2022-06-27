f8ec07088d8d4fb62685745202509fdaad9d490b
Benjamin Dupont <benjozorkfr@gmail.com>
2022-06-27 02:23:34 -0400

    add some content to the background service banner

--- src/renderer/components/AddonSection/StateSection.tsx
+++ src/renderer/components/AddonSection/StateSection.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react";
-import { ApplicationStatus, InstallingInstallStatuses, InstallStatus } from "renderer/components/AddonSection/Enums";
+import {
+    ApplicationStatus,
+    InstalledInstallStatuses,
+    InstallingInstallStatuses,
+    InstallStatus,
+} from "renderer/components/AddonSection/Enums";
 import { useAppSelector } from "renderer/redux/store";
 import { Addon, Publisher } from "renderer/utils/InstallerConfiguration";
 import { InstallingDependencyInstallState, InstallState } from "renderer/redux/features/installStatus";
 import { DownloadItem } from "renderer/redux/types";
-import { Broadcast, ExclamationTriangle } from "react-bootstrap-icons";
+import { Activity, ExclamationTriangle } from "react-bootstrap-icons";
 import { Resolver } from "renderer/utils/Resolver";
 import { BackgroundServices } from "renderer/utils/BackgroundServices";
+import { Button, ButtonType } from "renderer/components/Button";
+import { AlertModal, PromptModal, useModals } from "renderer/components/Modal";
+import { Toggle } from "renderer/components/Toggle";
 
 export interface StateSectionProps {
     publisher: Publisher,
@@ -37,7 +45,7 @@ export const StateSection: FC<StateSectionProps> = ({ publisher, addon }) => {
         dependencyAddonDownload = downloads.find((it) => it.id === dependencyAddonKey);
     }
 
-    const backgroundServiceBanner = useBackgroundServiceBanner(publisher, addon);
+    const backgroundServiceBanner = useBackgroundServiceBanner(publisher, addon, addonInstallState);
     const runningExternalAppBanner = useRunningExternalAppBanner(publisher, addon);
     const downloadProgressBanner = useDownloadProgressBanner(addon, dependencyAddonInstallState ?? addonInstallState, dependencyAddonDownload ?? addonDownload, isInstallingDependency);
 
@@ -59,7 +67,7 @@ export const StateSection: FC<StateSectionProps> = ({ publisher, addon }) => {
 const StateContainer: FC<{ visible: boolean }> = ({ visible, children }) => {
     return (
         <div
-            className="absolute h-32 bottom-0 left-0 flex flex-row items-end justify-between p-6 w-full bg-navy bg-opacity-80"
+            className="absolute h-32 bottom-0 left-0 flex flex-row items-end justify-between p-6 w-full bg-navy-dark bg-opacity-95"
             style={{ visibility: visible ? 'visible' : 'hidden' }}
         >
             {children}
@@ -78,40 +86,81 @@ const StateText: FC = ({ children }) => (
 interface ProgressBarProps {
     className: string,
     value: number,
+    animated?: boolean,
 }
 
-const ProgressBar: FC<ProgressBarProps> = ({ className, value }) => (
+const ProgressBar: FC<ProgressBarProps> = ({ className, value, animated = true }) => (
     <div className="absolute left-0 -bottom-1 w-full h-2 z-10 bg-black">
         <div
-            className={`absolute h-2 z-11 ${className} progress-bar-animated`}
+            className={`absolute h-2 z-11 ${className} ${animated ? 'progress-bar-animated' : ''}`}
             style={{ width: `${value}%` }}
         />
     </div>
 );
 
-const useBackgroundServiceBanner = (publisher: Publisher, addon: Addon): JSX.Element | undefined => {
-    if (addon.backgroundService) {
+const useBackgroundServiceBanner = (publisher: Publisher, addon: Addon, installState: InstallState): JSX.Element | undefined => {
+    const { showModalAsync } = useModals();
+
+    if (addon.backgroundService && InstalledInstallStatuses.includes(installState.status)) {
         const isRunning = BackgroundServices.checkIsRunning(addon, publisher);
 
-        if (isRunning) {
-            return (
-                <>
-                    <StateContainer visible={true}>
-                        <div className="flex gap-x-7 items-center">
-                            <Broadcast size={32} className="text-utility-green fill-current animate-pulse" />
+        const bgAccentColor = isRunning ? 'bg-utility-green' : 'bg-gray-500';
 
-                            <div className="flex flex-col gap-y-2">
-                                <SmallStateText>{addon.name}</SmallStateText>
-                                <StateText>{`Running`}</StateText>
-                            </div>
-                        </div>
-                    </StateContainer>
+        const handleClickAutostart = () => {
+            // let md = '';
+            // md += `You can choose to automatically start **${addon.name}** at login.\n\n`;
+            // md += `You can re-configure this again later.`;
 
-                    <ProgressBar className="bg-utility-green" value={100} />
-                </>
-
+            showModalAsync(
+                <AlertModal title={"Autostart Configuration"} bodyText={(
+                    <>
+                        <YesNoOptionToggle onToggle={() => {}} enabled={true} downloadSize="250" />
+                    </>
+                )} />,
             );
-        }
+        };
+
+        const handleStop = async () => {
+            const md = `Are you sure you want to shut down **${addon.name}**?. All related functionality will no longer be available.`;
+
+            const doStop = await showModalAsync(
+                <PromptModal title={"Are you sure?"} bodyText={md} confirmColor={ButtonType.Danger} />,
+            );
+
+            if (doStop) {
+                alert('bruh');
+            }
+        };
+
+        return (
+            <>
+                <StateContainer visible={true}>
+                    <div className="flex gap-x-7 items-center">
+                        {
+                            isRunning ? (
+                                <Activity size={32} className={`text-utility-green fill-current animate-pulse`} />
+                            ) : (
+                                <Activity size={32} className={`text-gray-500 fill-current`} />
+                            )
+                        }
+
+                        <div className="flex flex-col gap-y-2">
+                            <SmallStateText>{addon.name}</SmallStateText>
+                            <StateText>{isRunning ? 'Running' : 'Not Running'}</StateText>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-x-14 items-center">
+                        <span className="text-3xl text-quasi-white hover:text-cyan cursor-pointer" onClick={handleClickAutostart}>Autostart...</span>
+
+                        <Button className="w-64" type={ButtonType.Neutral} onClick={handleStop}>{isRunning ? 'Stop' : 'Start'}</Button>
+                    </div>
+                </StateContainer>
+
+                <ProgressBar className={bgAccentColor} value={100} animated={isRunning} />
+            </>
+
+        );
     }
 };
 
@@ -221,5 +270,31 @@ const useDownloadProgressBanner = (addon: Addon, installState: InstallState, dow
                 <ProgressBar className="bg-cyan" value={download.progress} />
             </StateContainer>
         </>
+    );
+};
+
+interface YesNoOptionToggleProps {
+    enabled: boolean,
+    onToggle: () => void
+    downloadSize?: string,
+}
+
+export const YesNoOptionToggle: FC<YesNoOptionToggleProps> = ({ enabled, onToggle, downloadSize }) => {
+    const handleClick = onToggle;
+
+    const bgColor = enabled ? 'bg-utility-green' : 'bg-navy-light';
+    const titleColor = enabled ? 'text-navy' : 'text-quasi-white';
+
+    return (
+        <div className={`flex items-center gap-x-10 ${bgColor} px-10 py-12 rounded-md transition-color duration-200 cursor-pointer`} onClick={handleClick}>
+            <Toggle value={enabled} onToggle={handleClick} scale={1.5} onColor={'bg-utility-green'} />
+
+            <span className="flex gap-x-20">
+                <span className={`font-manrope font-bold text-4xl ${titleColor}`}>Terrain Database</span>
+                {downloadSize && (
+                    <span className={`font-manrope font-semibold text-4xl ${titleColor}`}>{downloadSize}</span>
+                )}
+            </span>
+        </div>
     );
 };
