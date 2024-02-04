e70fe8bd1bc6daf3d493ba1783dfa815b7e578a3
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-02-04 10:57:28 -0500

    refactor: add react-hooks and tailwind lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,14 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['prettier', 'react', '@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'prettier', 'react', 'tailwindcss'],
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+  ],
   env: {
     browser: true,
     es2021: true,
@@ -10,6 +16,9 @@ module.exports = {
   },
   parserOptions: {
     sourceType: 'module',
+  },
+  settings: {
+    react: { version: 'detect' },
   },
   rules: {
     'prettier/prettier': [
@@ -20,5 +29,11 @@ module.exports = {
       },
     ],
     'no-console': 'off',
+
+    'tailwindcss/classnames-order': 'error',
+    'tailwindcss/enforces-negative-arbitrary-values': 'error',
+    'tailwindcss/enforces-shorthand': 'error',
+    'tailwindcss/migration-from-tailwind-2': 'error',
+    'tailwindcss/no-contradicting-classname': 'error',
   },
 };
