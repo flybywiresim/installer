3bd1bb434d536172866a0182ef38db69105b1f0f
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-02-25 16:43:29 -0500

    style: run lint

--- src/renderer/components/SettingsSection/Developer.tsx
+++ src/renderer/components/SettingsSection/Developer.tsx
@@ -1,53 +1,52 @@
 import React, { FC, useEffect, useState } from 'react';
-import { useSetting } from "common/settings";
+import { useSetting } from 'common/settings';
 
 const SettingsItem: FC<{ name: string }> = ({ name, children }) => (
-    <div className="flex flex-row items-center justify-between py-3.5">
-        <p className="m-0">{name}</p>
-        {children}
-    </div>
+  <div className="flex flex-row items-center justify-between py-3.5">
+    <p className="m-0">{name}</p>
+    {children}
+  </div>
 );
 
 const index = (): JSX.Element => {
-    const [configDownloadUrl, setConfigDownloadUrl] = useSetting<string>('mainSettings.configDownloadUrl');
-    const [configDownloadUrlValid, setConfigDownloadUrlValid] = useState<boolean>(false);
+  const [configDownloadUrl, setConfigDownloadUrl] = useSetting<string>('mainSettings.configDownloadUrl');
+  const [configDownloadUrlValid, setConfigDownloadUrlValid] = useState<boolean>(false);
 
-    const validateUrl = () => {
-        try {
-            fetch(configDownloadUrl)
-                .then((response) => {
-                    setConfigDownloadUrlValid(response.status === 200);
-                });
-        } catch (e) {
-            setConfigDownloadUrlValid(false);
-        }
-    };
+  const validateUrl = () => {
+    try {
+      fetch(configDownloadUrl).then((response) => {
+        setConfigDownloadUrlValid(response.status === 200);
+      });
+    } catch (e) {
+      setConfigDownloadUrlValid(false);
+    }
+  };
 
-    useEffect(() => {
-        validateUrl();
-    }, []);
+  useEffect(() => {
+    validateUrl();
+  }, []);
 
-    return (
-        <div>
-            <div className="flex flex-col">
-                <h2 className="text-white">General Settings</h2>
-                <div className="flex flex-col divide-y divide-gray-600">
-                    <SettingsItem name="Configuration Download URL">
-                        <div className='flex flex-row items-center justify-between text-white py-4'>
-                            <input
-                                className={` text-xl text-right ${configDownloadUrlValid ? 'text-green-500' : 'text-red-500'}`}
-                                value={configDownloadUrl}
-                                type="url"
-                                onChange={(event) => setConfigDownloadUrl(event.target.value)}
-                                onBlur={() => validateUrl()}
-                                size={50}
-                            />
-                        </div>
-                    </SettingsItem>
-                </div>
+  return (
+    <div>
+      <div className="flex flex-col">
+        <h2 className="text-white">General Settings</h2>
+        <div className="flex flex-col divide-y divide-gray-600">
+          <SettingsItem name="Configuration Download URL">
+            <div className="flex flex-row items-center justify-between py-4 text-white">
+              <input
+                className={` text-right text-xl ${configDownloadUrlValid ? 'text-green-500' : 'text-red-500'}`}
+                value={configDownloadUrl}
+                type="url"
+                onChange={(event) => setConfigDownloadUrl(event.target.value)}
+                onBlur={() => validateUrl()}
+                size={50}
+              />
             </div>
+          </SettingsItem>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default index;
--- src/renderer/components/Toggle.tsx
+++ src/renderer/components/Toggle.tsx
@@ -1,15 +1,28 @@
 import React, { FC } from 'react';
 
 interface ToggleProps {
-    value: boolean;
-    onToggle: (value: boolean) => void;
-    scale?: number;
-    bgColor?: string,
-    onColor?: string;
+  value: boolean;
+  onToggle: (value: boolean) => void;
+  scale?: number;
+  bgColor?: string;
+  onColor?: string;
 }
 
-export const Toggle: FC<ToggleProps> = ({ value, onToggle, scale = 1, bgColor = 'bg-navy-light', onColor = 'bg-cyan-medium' }) => (
-    <div className={`flex text-3xl items-center w-14 h-8 rounded-full cursor-pointer ${bgColor}`} onClick={() => onToggle(!value)} style={{ transform: `scale(${scale})` }}>
-        <div className={`w-6 h-6 bg-gray-400 rounded-full transition mx-1.5 duration-200 transform ${value && `${onColor}`}`} style={{ transform: `translate(${value ? '12px' : '1px'}, 0)` }} />
-    </div>
+export const Toggle: FC<ToggleProps> = ({
+  value,
+  onToggle,
+  scale = 1,
+  bgColor = 'bg-navy-light',
+  onColor = 'bg-cyan-medium',
+}) => (
+  <div
+    className={`flex h-8 w-14 cursor-pointer items-center rounded-full text-3xl ${bgColor}`}
+    onClick={() => onToggle(!value)}
+    style={{ transform: `scale(${scale})` }}
+  >
+    <div
+      className={`mx-1.5 size-6 rounded-full bg-gray-400 transition duration-200${value && `${onColor}`}`}
+      style={{ transform: `translate(${value ? '12px' : '1px'}, 0)` }}
+    />
+  </div>
 );
