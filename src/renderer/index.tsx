15442ca0f68596e2145caea990e727388f9859e5
Benjamin Dupont <4503241+Benjozork@users.noreply.github.com>
2024-04-21 23:38:43 -0400

    chore: update some packages

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,34 +1,7 @@
-/**
- * This file will automatically be loaded by webpack and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Sentry from '@sentry/electron/renderer';
-import { BrowserTracing } from '@sentry/tracing';
+import { browserTracingIntegration } from '@sentry/browser';
 import { Provider } from 'react-redux';
 import App, { fetchLatestVersionNames } from 'renderer/components/App';
 import { Configuration, InstallerConfiguration } from 'renderer/utils/InstallerConfiguration';
@@ -48,12 +21,10 @@ import 'simplebar-react/dist/simplebar.min.css';
 import './index.scss';
 import { Button, ButtonType } from 'renderer/components/Button';
 
-console.log('a');
-
 // Setup Sentry
 Sentry.init({
   release: packageJson.version,
-  integrations: [new BrowserTracing()],
+  integrations: [browserTracingIntegration()],
   tracesSampleRate: 1.0,
   beforeBreadcrumb: (event) => {
     if (event.category === 'fetch' && event.data.url.startsWith('http://localhost')) {
