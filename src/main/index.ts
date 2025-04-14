96698f291e8fdeac52cb721a3c79addae0cf18b8
FoxtrotSierra <github@florian-scheuner.de>
2025-04-14 22:01:25 +0300

    fix: remove undesired changes

--- src/main/index.ts
+++ src/main/index.ts
@@ -222,65 +222,6 @@ function initializeApp() {
 
   Menu.setApplicationMenu(null);
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', () => {
-    createWindow();
-
-    if (import.meta.env.DEV) {
-      installExtension(REACT_DEVELOPER_TOOLS)
-        .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
-
-      installExtension(REDUX_DEVTOOLS)
-        .then((name) => console.log(`Added Extension:  ${name}`))
-        .catch((err) => console.log('An error occurred: ', err));
-    }
-
-    //Register keybinds
-    mainWindow.webContents.on('before-input-event', (event, input) => {
-      // Check if the input event is for window reloading
-      if (
-        input.type === 'keyUp' &&
-        (input.key.toLowerCase() === 'r' || input.key === 'F5') &&
-        (input.control || input.meta)
-      ) {
-        mainWindow.isFocused() && mainWindow.reload();
-      }
-
-      // Check if the input even is for dev tools
-      if (input.type === 'keyUp' && input.key === 'F12' && (input.control || input.meta)) {
-        mainWindow.isFocused() && mainWindow.webContents.toggleDevTools();
-      }
-    });
-  });
-
-  // Quit when all windows are closed, except on macOS. There, it's common
-  // for applications and their menu bar to stay active until the user quits
-  // explicitly with Cmd + Q.
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-
-  app.setAppUserModelId('FlyByWire Installer');
-
-  let mainWindow: BrowserWindow;
-
-  Menu.setApplicationMenu(null);
-
-  const serve = process.argv.slice(1).some((arg) => arg === '--serve');
-
   const disableGpu = process.argv.slice(1).some((arg) => arg === '--disable-gpu');
 
   if (disableGpu) {
