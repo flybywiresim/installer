c6d1182e8c761d07cb6b716b6e9563654d7a9647
FoxtrotSierra <github@florian-scheuner.de>
2025-04-14 22:09:52 +0300

    refactor: prettier advanced settings

--- src/renderer/components/SettingsSection/Advanced.tsx
+++ src/renderer/components/SettingsSection/Advanced.tsx
@@ -1,80 +1,73 @@
 import React from 'react';
-import settings, { useSetting } from "common/settings";
+import settings, { useSetting } from 'common/settings';
 import { Toggle } from '../Toggle';
 import { SettingItemProps, SettingsItem } from './General';
 
 const DisableHardwareAcceleration = ({ value, setValue }: SettingItemProps<boolean>) => {
-    const handleClick = (value: boolean) => {
-        settings.set('advanced.disableHardwareAcceleration', value);
-        setValue(value);
-    };
+  const handleClick = (value: boolean) => {
+    settings.set('advanced.disableHardwareAcceleration', value);
+    setValue(value);
+  };
 
-    return (
-        <SettingsItem name="Disable Hardware Acceleration">
-            <Toggle
-                value={value}
-                onToggle={handleClick}
-            />
-        </SettingsItem>
-    );
+  return (
+    <SettingsItem name="Disable Hardware Acceleration">
+      <Toggle value={value} onToggle={handleClick} />
+    </SettingsItem>
+  );
 };
 
 const DisableGpuSandbox = ({ value, setValue }: SettingItemProps<boolean>) => {
-    const handleClick = (value: boolean) => {
-        settings.set('advanced.disableGpuSandbox', value);
-        setValue(value);
-    };
+  const handleClick = (value: boolean) => {
+    settings.set('advanced.disableGpuSandbox', value);
+    setValue(value);
+  };
 
-    return (
-        <SettingsItem name="Disable GPU Sandbox">
-            <Toggle
-                value={value}
-                onToggle={handleClick}
-            />
-        </SettingsItem>
-    );
+  return (
+    <SettingsItem name="Disable GPU Sandbox">
+      <Toggle value={value} onToggle={handleClick} />
+    </SettingsItem>
+  );
 };
 
 const DisableSandbox = ({ value, setValue }: SettingItemProps<boolean>) => {
-    const handleClick = (value: boolean) => {
-        settings.set('advanced.noSandbox', value);
-        setValue(value);
-    };
+  const handleClick = (value: boolean) => {
+    settings.set('advanced.noSandbox', value);
+    setValue(value);
+  };
 
-    return (
-        <SettingsItem name="Disable Sandbox">
-            <Toggle
-                value={value}
-                onToggle={handleClick}
-            />
-        </SettingsItem>
-    );
+  return (
+    <SettingsItem name="Disable Sandbox">
+      <Toggle value={value} onToggle={handleClick} />
+    </SettingsItem>
+  );
 };
 
 const index = (): JSX.Element => {
-    const [showAll] = useSetting<boolean>('advanced.showAll');
-    const showAllSettings = showAll || process.env.NODE_ENV === 'development';
-    const [disableHardwareAcceleration, setDisableHardwareAcceleration] = useSetting<boolean>('advanced.disableHardwareAcceleration');
-    const [disableGpuSandbox, setDisableGpuSandbox] = useSetting<boolean>('advanced.disableGpuSandbox');
-    const [disableSandbox, setDisableSandbox] = useSetting<boolean>('advanced.noSandbox');
+  const [showAll] = useSetting<boolean>('advanced.showAll');
+  const showAllSettings = showAll || process.env.NODE_ENV === 'development';
+  const [disableHardwareAcceleration, setDisableHardwareAcceleration] = useSetting<boolean>(
+    'advanced.disableHardwareAcceleration',
+  );
+  const [disableGpuSandbox, setDisableGpuSandbox] = useSetting<boolean>('advanced.disableGpuSandbox');
+  const [disableSandbox, setDisableSandbox] = useSetting<boolean>('advanced.noSandbox');
 
-    return (
-        <div>
-            <div className="flex flex-col">
-                <h2 className="text-red">Advanced Settings</h2>
-                <div className="flex flex-col divide-y divide-gray-600">
-                    <DisableHardwareAcceleration value={disableHardwareAcceleration} setValue={setDisableHardwareAcceleration} />
-                    { (disableGpuSandbox || showAllSettings) &&
-                        <DisableGpuSandbox value={disableGpuSandbox} setValue={setDisableGpuSandbox} />
-                    }
-                    { (disableSandbox || showAllSettings) &&
-                        <DisableSandbox value={disableSandbox} setValue={setDisableSandbox} />
-                    }
-                    <p className="text-red">It is required to restart the application to apply changes</p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="flex flex-col">
+        <h2 className="text-red">Advanced Settings</h2>
+        <div className="flex flex-col divide-y divide-gray-600">
+          <DisableHardwareAcceleration value={disableHardwareAcceleration} setValue={setDisableHardwareAcceleration} />
+          {(disableGpuSandbox || showAllSettings) && (
+            <DisableGpuSandbox value={disableGpuSandbox} setValue={setDisableGpuSandbox} />
+          )}
+          {(disableSandbox || showAllSettings) && (
+            <DisableSandbox value={disableSandbox} setValue={setDisableSandbox} />
+          )}
+          <p className="text-red">It is required to restart the application to apply changes</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default index;
